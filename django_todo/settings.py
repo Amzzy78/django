347bf5650149cbc6a6c28be0ca03f28a82ad049e
Amzzy78 <murphy.annmarie78@gmail.com>
2021-12-10 22:04:19 +0000

    Change the database if statement

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -32,7 +32,7 @@ DEBUG = development
 
 if development:
     ALLOWED_HOSTS = ['localhost']
-else:    
+else: 
     ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
 
 
@@ -78,24 +78,20 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'django_todo.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-if development:
+if os.environ.get("DATABASE_URL"):
     DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR / 'db.sqlite3'),
-        }
+        "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
     }
 else:
-
     DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
-
-
-
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        }
+    }
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
