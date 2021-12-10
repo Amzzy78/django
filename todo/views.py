e383f388951250837f3e1c242abc4234b9714dd8
Amzzy78 <murphy.annmarie78@gmail.com>
2021-12-10 13:16:56 +0000

    Troubleshoot error in code

--- todo/views.py
+++ todo/views.py
@@ -2,8 +2,6 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import Item
 from .forms import ItemForm
 
-# Create your views here.
-
 
 def get_todo_list(request):
     items = Item.objects.all()
@@ -13,17 +11,16 @@ def get_todo_list(request):
     return render(request, 'todo/todo_list.html', context)
 
 
-def add_item(request):  
+def add_item(request):
     if request.method == 'POST':
         form = ItemForm(request.POST)
         if form.is_valid():
             form.save()
-
-        return redirect('get_todo_list')
+            return redirect('get_todo_list')
     form = ItemForm()
     context = {
         'form': form
-    }    
+    }
     return render(request, 'todo/add_item.html', context)
 
 
@@ -33,13 +30,11 @@ def edit_item(request, item_id):
         form = ItemForm(request.POST, instance=item)
         if form.is_valid():
             form.save()
-
-        return redirect('get_todo_list')
+            return redirect('get_todo_list')
     form = ItemForm(instance=item)
     context = {
         'form': form
     }
-
     return render(request, 'todo/edit_item.html', context)
 
 
@@ -54,4 +49,3 @@ def delete_item(request, item_id):
     item = get_object_or_404(Item, id=item_id)
     item.delete()
     return redirect('get_todo_list')
-    
